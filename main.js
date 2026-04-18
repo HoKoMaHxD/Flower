f4f7ec33f40697e100cd7033190256281e23f129
HoKoMaHxD <109537378+HoKoMaHxD@users.noreply.github.com>
2026-04-18 11:55:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-window.onload = () => {
-    // التأخير ثانية واحدة لضمان تحميل العناصر ثم بدء الأنيميشن
-    const timeout = setTimeout(() => {
-        document.body.classList.remove("not-loaded");
-        clearTimeout(timeout);
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
     }, 1000);
-
-    console.log("Vercel Deployment: Spotify & Flowers Active.");
-};
+  };
