d67103dbf40ae083c94f6c75951ec63678e8bc22
HoKoMaHxD <109537378+HoKoMaHxD@users.noreply.github.com>
2026-04-18 11:38:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 onload = () => {
+    // الانتظار لمدة ثانية واحدة قبل بدء أنيميشن تفتح الزهور
     const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
+        document.body.classList.remove("not-loaded");
+        clearTimeout(c);
     }, 1000);
-  };
+
+    // ملاحظة احترافية: 
+    // إذا كنت تريد التفاعل مع مشغل سبوتيفاي برمجياً مستقبلاً 
+    // يمكنك إضافة الأكواد هنا.
+    console.log("Flowers are blooming...");
+};
